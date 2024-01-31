cdc01dbb6136110746d59c7db50ff81f8485fb82
Tiege McCarthy <tiegem@utas.edu.au>
2024-01-31 15:28:27 +1100

    Added more to the report generation script. Including move from mjd to YYYY:DOY for times.

--- summaryGenerator.py
+++ summaryGenerator.py
@@ -33,7 +33,7 @@ def extractStationData(station_code, database_name, mjd_start, mjd_stop, search=
     cursor = conn.cursor()
     query = "USE " + database_name +";"
     cursor.execute(query)
-    query = "SELECT ExpID, Date, Date_MJD, Performance, Performance_UsedVsRecov, W_RMS_del, Detect_Rate_X, Detect_Rate_S, Total_Obs FROM " + station_code+ " WHERE ExpID LIKE \"" + search + "\" AND Date_MJD > " + str(mjd_start) + " AND Date_MJD < " + str(mjd_stop) + " ORDER BY DATE ASC;"
+    query = "SELECT ExpID, Date, Date_MJD, Performance, Performance_UsedVsRecov, W_RMS_del, Detect_Rate_X, Detect_Rate_S, Total_Obs, Problem_String FROM " + station_code+ " WHERE ExpID LIKE \"" + search + "\" AND Date_MJD > " + str(mjd_start) + " AND Date_MJD < " + str(mjd_stop) + " ORDER BY DATE ASC;"
     cursor.execute(query)
     result = cursor.fetchall()
     return result
@@ -58,7 +58,7 @@ def wRmsAnalysis(results):
     ax.set_title('Station W.RMS vs. Time')
     ax.set_xlim([np.min(time_data), np.max(time_data)])
     ax.tick_params(axis='x', labelrotation=90)
-    plt.savefig('wRMS.png')
+    plt.savefig('wRMS.png', bbox_inches="tight")
     return ax
 
 def performanceAnalysis(results):
@@ -81,7 +81,7 @@ def performanceAnalysis(results):
     ax.set_ylim([0, 1.0])
     ax.set_xlim([np.min(time_data), np.max(time_data)])
     ax.tick_params(axis='x', labelrotation=90)
-    plt.savefig('performance.png')
+    plt.savefig('performance.png', bbox_inches="tight")
     return ax
 
 def usedVsRecoveredAnalysis(results):
@@ -131,30 +131,56 @@ def detectRate(results, band):
     ax.set_ylim([0, 1.0])
     ax.set_xlim([np.min(time_data), np.max(time_data)])
     ax.tick_params(axis='x', labelrotation=90)
-    plt.savefig(band + '_detect_rate.png')
+    plt.savefig(band + '_detect_rate.png', bbox_inches="tight")
     return ax, rate_str
 
-def generatePDF(pdf_name, station, str1, str2, str3, str4, str5):
+def problemExtract(results):
+    table = Table(rows=results)
+    bad_data = []
+    for i in range(0, len(table['col9'])):
+        if table['col9'][i] == '' or table['col9'][i] == None:
+            bad_data.append(i)
+    table.remove_rows(bad_data)
+    problem_list = []
+    for j in range(0,len(table['col9'])):
+        problem = table['col0'][j] + ': ' + table['col9'][j]
+        problem_list.append(problem)
+    return problem_list
+
+
+def generatePDF(pdf_name, station, str1, str2, str3, str4, str5, problem_string):
+    # Page 1
     report = Canvas(pdf_name)
     report.setFont('Helvetica-Bold', 12)
     report.drawString(50, 780, station + ' station report')
-    report.setFont('Helvetica', 10)
     t1 = report.beginText()
+    t1.setFont('Helvetica', 10)
     t1.setTextOrigin(50, 750)
     t1.textLines(str1 + str2 + str3 + '\n' + str4 + '\n' + str5)
     report.drawText(t1)
-    report.drawInlineImage( 'X_detect_rate.png', 35, 330, width=280, preserveAspectRatio=True)
-    report.drawInlineImage( 'S_detect_rate.png', 295, 330, width=280, preserveAspectRatio=True)
-    report.drawInlineImage( 'wRMS.png', 35, 110, width=280, preserveAspectRatio=True)
-    report.drawInlineImage( 'performance.png', 295, 110, width=280, preserveAspectRatio=True)
+    report.drawInlineImage( 'X_detect_rate.png', 22, 320, width=280, preserveAspectRatio=True)
+    report.drawInlineImage( 'S_detect_rate.png', 300, 320, width=280, preserveAspectRatio=True)
+    report.drawInlineImage( 'wRMS.png', 22, 75, width=280, preserveAspectRatio=True)
+    report.drawInlineImage( 'performance.png', 300, 75, width=280, preserveAspectRatio=True)
+    report.showPage()
+    # Page 2
+    report.drawString(50, 780, "Reported issues (as extracted from analysis reports)")
+    t2 = report.beginText()
+    t2.setTextOrigin(50, 750)
+    t2.setFont('Helvetica', 8)
+    for line in problem_string:
+        t2.textLines(line)
+    report.drawText(t2)
     report.save()    
 
 
-def main(stat_code, db_name, mjd_start, mjd_stop, search='%'):
-    result = extractStationData(stat_code, db_name, mjd_start, mjd_stop, search)
+def main(stat_code, db_name, start, stop, search='%'):
+    start_time = Time(start, format='yday', out_subfmt='date')
+    stop_time = Time(stop, format='yday', out_subfmt='date')
+    result = extractStationData(stat_code, db_name, start_time.mjd, stop_time.mjd, search)
     table = Table(rows=result)
     time_data = Column(table['col1'], dtype=Time)
-    intro_str = stat_code + ' data extracted for time range MJD ' + str(Time(mjd_start, format='mjd')) + " through MJD " + str(mjd_stop) + "..."
+    intro_str = stat_code + ' data extracted for time range ' + start_time.iso + " through " + stop_time.iso + "..."
     tot_sess_str = "\nTotal number of " + str(stat_code) + " sessions matching search criteria: " + str(len(table['col4']))
     tot_obs_str = "\nTotal number of " + str(stat_code) + " observations across all sessions matching search criteria: " + str(np.nansum(table['col8'].astype(float)).astype(int))
     print(intro_str + tot_sess_str + tot_obs_str)
@@ -163,9 +189,11 @@ def main(stat_code, db_name, mjd_start, mjd_stop, search='%'):
     ax_four, str4 = detectRate(result, 'X')
     ax_five, str5 = detectRate(result, 'S')
     # Make the PDF report
+    problems = problemExtract(result)
     print('Generating PDF report...')
-    generatePDF('test.pdf', stat_code, intro_str, tot_sess_str, tot_obs_str, str4, str5)
+    generatePDF('test.pdf', stat_code, intro_str, tot_sess_str, tot_obs_str, str4, str5, problems)
     plt.show()
+    return
     
 if __name__ == '__main__':
     args = parseFunc()
