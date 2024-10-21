fd26da058bbbcd995b417706be376454c04188fb
Tiege McCarthy <tiegem@utas.edu.au>
2024-10-21 12:28:04 +1100

    Added a 3rd page to the report that includes the tabulated data.

--- summaryGenerator.py
+++ summaryGenerator.py
@@ -3,6 +3,7 @@
 import MySQLdb as mariadb
 from astropy.table import vstack, Table, Column
 from astropy.time import Time
+from astropy.io import ascii
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -10,6 +11,7 @@ from reportlab.pdfgen.canvas import Canvas
 from datetime import datetime
 from adjustText import adjust_text
 import textwrap
+import pandas as pd
 
 
 
@@ -40,23 +42,24 @@ def extractStationData(station_code, database_name, mjd_start, mjd_stop, search=
     query = "SELECT ExpID, Date, Date_MJD, Performance, Performance_UsedVsRecov, W_RMS_del, Detect_Rate_X, Detect_Rate_S, Total_Obs, Notes, Pos_X, Pos_Y, Pos_Z, Pos_E, Pos_N, Pos_U FROM " + station_code+ " WHERE ExpID LIKE \"" + search + "\" AND Date_MJD > " + str(mjd_start) + " AND Date_MJD < " + str(mjd_stop) + " ORDER BY DATE ASC;"
     cursor.execute(query)
     result = cursor.fetchall()
-    return result
+    col_names = ["ExpID", "Date", "Date_MJD", "Performance", "Performance_UsedVsRecov", "W_RMS_del", "Detect_Rate_X", "Detect_Rate_S", "Total_Obs", "Notes", "Pos_X", "Pos_Y", "Pos_Z", "Pos_E", "Pos_N", "Pos_U"]
+    return result, col_names 
 
-def wRmsAnalysis(results):
-    table = Table(rows=results)
+def wRmsAnalysis(table_input):
+    table = table_input.copy()
     # filter dummy data
     bad_data = []
-    for i in range(0, len(table['col5'])):
-        if table['col5'][i] == -999:
+    for i in range(0, len(table['W_RMS_del'])):
+        if table['W_RMS_del'][i] == -999:
             bad_data.append(i)
     table.remove_rows(bad_data)
-    time_data = Column(table['col1'], dtype=Time)
+    time_data = Column(table['Date'], dtype=Time)
     #print("Number of sessions: " + str(len(table['col5'])))
-    wrms_med_str = "Median station W.RMS over period: " + str(np.median(table['col5'])) + " ps"
+    wrms_med_str = "Median station W.RMS over period: " + str(np.median(table['W_RMS_del'])) + " ps"
     print(wrms_med_str)
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(time_data, table['col5'], color='k', s=20)
+    ax.scatter(time_data, table['W_RMS_del'], color='k', s=20)
     #ax.plot(mjd_x, wrms_runavg, color='r')
     ax.set_xlabel('Date')
     ax.set_ylabel('W.RMS (ps)')
@@ -67,21 +70,21 @@ def wRmsAnalysis(results):
     plt.savefig('wRMS.png', bbox_inches="tight")
     return ax, wrms_med_str
 
-def performanceAnalysis(results):
-    table = Table(rows=results)
+def performanceAnalysis(table_input):
+    table = table_input.copy()
     # filter sessions with 0% data
     bad_data = []
-    for i in range(0, len(table['col3'])):
-        if table['col3'][i] == 0:
+    for i in range(0, len(table['Performance'])):
+        if table['Performance'][i] == 0:
             bad_data.append(i)
     table.remove_rows(bad_data)
-    time_data = Column(table['col1'], dtype=Time)
-    perf_str = "Median station 'Performance' (used/scheduled) over period: " + str(np.median(table['col3']))
+    time_data = Column(table['Date'], dtype=Time)
+    perf_str = "Median station 'Performance' (used/scheduled) over period: " + str(np.median(table['Performance']))
     print(perf_str)
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(time_data, table['col3'], color='k', s=10, marker='s')
-    ax.fill_between(time_data, table['col3'], alpha = 0.5)
+    ax.scatter(time_data, table['Performance'], color='k', s=10, marker='s')
+    ax.fill_between(time_data, table['Performance'], alpha = 0.5)
     #ax.plot(mjd_x, wrms_runavg, color='r')
     ax.set_title('Performance (used/scheduled) vs. Time')
     ax.set_xlabel('Date')
@@ -92,27 +95,27 @@ def performanceAnalysis(results):
     plt.savefig('performance.png', bbox_inches="tight")
     return ax, perf_str
 
-def posAnalysis(results, coord):
+def posAnalysis(table_input, coord):
+    table = table_input.copy()
     if coord == 'X':
-        col_name = 'col10'
+        col_name = 'Pos_X'
     elif coord == 'Y':
-        col_name = 'col11'
+        col_name = 'Pos_Y'
     elif coord == 'Z':
-        col_name = 'col12'
+        col_name = 'Pos_Z'
     elif coord == 'E':
-        col_name = 'col13'
+        col_name = 'Pos_E'
     elif coord == 'N':
-        col_name = 'col14'
+        col_name = 'Pos_N'
     elif coord == 'U':
-        col_name = 'col15'
-    table = Table(rows=results)
+        col_name = 'Pos_U'
     # filter sessions with 0% data
     bad_data = []
     for i in range(0, len(table[col_name])):
         if table[col_name][i] == 0:
             bad_data.append(i)
     table.remove_rows(bad_data)
-    time_data = Column(table['col1'], dtype=Time)
+    time_data = Column(table['Date'], dtype=Time)
     fig = plt.figure()
     ax = fig.add_subplot(111)
     lim_offset = np.median(table[col_name])
@@ -128,21 +131,21 @@ def posAnalysis(results, coord):
     plt.savefig(coord + '_pos.png', bbox_inches="tight")
     return ax
 
-def usedVsRecoveredAnalysis(results):
-    table = Table(rows=results)
+def usedVsRecoveredAnalysis(table_input):
+    table = table_input.copy()
     # filter sessions with 0% data
     bad_data = []
-    for i in range(0, len(table['col4'])):
-        if table['col4'][i] == 0 or table['col4'][i] == None:
+    for i in range(0, len(table['Performance_UsedVsRecov'])):
+        if table['Performance_UsedVsRecov'][i] == 0 or table['Performance_UsedVsRecov'][i] == None:
             bad_data.append(i)
     table.remove_rows(bad_data)
-    time_data = Column(table['col1'], dtype=Time)
+    time_data = Column(table['Date'], dtype=Time)
     #print("Number of sessions: " + str(len(table['col4'])))
     #print("Median used vs recovered observations: " + str(np.median(table['col4'])))
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(time_data, table['col4'], color='k', s=5)
-    ax.fill_between(time_data, table['col4'], alpha = 0.5)
+    ax.scatter(time_data, table['v'], color='k', s=5)
+    ax.fill_between(time_data, table['Performance_UsedVsRecov'], alpha = 0.5)
     ax.set_title('Fractional Used/Recovered Observations vs. Time')
     ax.set_xlabel('MJD (days)')
     ax.set_ylim([0, 1.0])
@@ -150,19 +153,19 @@ def usedVsRecoveredAnalysis(results):
     ax.tick_params(axis='x', labelrotation=90)
     return ax
 
-def detectRate(results, band):
+def detectRate(table_input, band):
+    table = table_input.copy()
     if band == 'X':
-        col_name = 'col6'
+        col_name = 'Detect_Rate_X'
     elif band == 'S':
-        col_name = 'col7'
-    table = Table(rows=results)
+        col_name = 'Detect_Rate_S'
     # filter sessions with 0% data
     bad_data = []
     for i in range(0, len(table[col_name])):
         if table[col_name][i] == 0 or table[col_name][i] == None:
             bad_data.append(i)
     table.remove_rows(bad_data)
-    time_data = Column(table['col1'], dtype=Time)
+    time_data = Column(table['Date'], dtype=Time)
     rate_str = "Median " + band + "-band detection rate: " + str(np.median(table[col_name]))
     print(rate_str)
     fig = plt.figure()
@@ -184,19 +187,19 @@ def detectRate(results, band):
     plt.savefig(band + '_detect_rate.png', bbox_inches="tight")
     return ax, rate_str
 
-def problemExtract(results):
+def problemExtract(table_input):
+    table = table_input.copy()
     problem_flag = ['pcal', 'phase', 'bad', 'lost', 'clock', 
                     'error', ' late ', 'issue', 'sensitivity',
                     'minus', 'removed']
-    table = Table(rows=results)
     bad_data = []
-    for i in range(0, len(table['col9'])):
-        if table['col9'][i] == '' or table['col9'][i] == None:
+    for i in range(0, len(table['Notes'])):
+        if table['Notes'][i] == '' or table['Notes'][i] == None:
             bad_data.append(i)
     table.remove_rows(bad_data)
     problem_list = []
-    for j in range(0,len(table['col9'])):
-        problem = table['col0'][j].upper() + ': ' + table['col9'][j]
+    for j in range(0,len(table['Notes'])):
+        problem = table['ExpID'][j].upper() + ': ' + table['Notes'][j]
         problem = problem.replace("Applied manual phase calibration", "")
         if any(element in problem.lower() for element in problem_flag): # see if a 'problem' flag is present in the notes
             #if not "manual phase calibration" in problem.lower(): # filter out the generic manual pcal notes
@@ -204,7 +207,7 @@ def problemExtract(results):
             problem_list.append(problem)
     return problem_list
 
-def generatePDF(pdf_name, start, stop, station, str2, str3, str4, str5, problem_string):
+def generatePDF(pdf_name, start, stop, station, str2, str3, str4, str5, problem_string, table):
     # Page 1
     report = Canvas(pdf_name)
     report.setFont('Helvetica-Bold', 20)
@@ -225,10 +228,20 @@ def generatePDF(pdf_name, start, stop, station, str2, str3, str4, str5, problem_
     report.drawString(50, 780, "Reported issues (as extracted from correlation reports)")
     t2 = report.beginText()
     t2.setTextOrigin(50, 750)
-    t2.setFont('Helvetica', 5)
+    t2.setFont('Helvetica', 6)
     for line in problem_string:
         t2.textLines(line)
     report.drawText(t2)
+    report.showPage()
+    # Page 3
+    report.setFont('Helvetica-Bold', 12)
+    report.drawString(50, 780, "Tabulated data")
+    t3 = report.beginText()
+    t3.setTextOrigin(50, 750)
+    t3.setFont('Courier', 6)
+    for line in table:
+        t3.textLines(line)
+    report.drawText(t3)
     report.showPage()
     report.save()
 
@@ -243,29 +256,33 @@ def text_plotter(x_data, y_data, text_positions, axis,txt_width,txt_height):
 def main(stat_code, db_name, start, stop, output_name, search='%'):
     start_time = Time(start, format='yday', out_subfmt='date')
     stop_time = Time(stop, format='yday', out_subfmt='date')
-    result = extractStationData(stat_code, db_name, start_time.mjd, stop_time.mjd, search)
-    table = Table(rows=result)
+    result, col_names = extractStationData(stat_code, db_name, start_time.mjd, stop_time.mjd, search)
+    table = Table(rows=result, names=col_names)
     #time_data = Column(table['col1'], dtype=Time)
     intro_str = stat_code + ' data extracted for time range: ' + start_time.iso + " through " + stop_time.iso
-    tot_sess_str = "\nTotal number of " + str(stat_code) + " sessions found in database for this time range: " + str(len(table['col4']))
-    tot_obs_str = "\nTotal number of " + str(stat_code) + " observations across all sessions in this time range: " + str(np.nansum(table['col8'].astype(float)).astype(int))
+    tot_sess_str = "\nTotal number of " + str(stat_code) + " sessions found in database for this time range: " + str(len(table['ExpID']))
+    tot_obs_str = "\nTotal number of " + str(stat_code) + " observations across all sessions in this time range: " + str(np.nansum(table['Total_Obs'].astype(float)).astype(int))
     print(intro_str + tot_sess_str + tot_obs_str)
-    ax_two, wrms_str = wRmsAnalysis(result)
-    ax_one, perf_str = performanceAnalysis(result)
+    ax_two, wrms_str = wRmsAnalysis(table)
+    ax_one, perf_str = performanceAnalysis(table)
     #ax_four, detectX_str = detectRate(result, 'X')
     #ax_four, detectS_str = detectRate(result, 'S')
     #ax_five,  = detectRate(result, 'S')
     #ax_six = posAnalysis(result, 'X')
     #ax_seven = posAnalysis(result, 'Y')
     #ax_eight = posAnalysis(result, 'Z')
-    ax_nine = posAnalysis(result, 'E')
-    ax_ten = posAnalysis(result, 'N')
-    ax_eleven = posAnalysis(result, 'U')
+    ax_nine = posAnalysis(table, 'E')
+    ax_ten = posAnalysis(table, 'N')
+    ax_eleven = posAnalysis(table, 'U')
     # Make the PDF report
-    problems = problemExtract(result)
+    problems = problemExtract(table)
     print('Generating PDF report...')
-    generatePDF(output_name, start_time, stop_time, stat_code, tot_sess_str, tot_obs_str, wrms_str, perf_str, problems)
-    #plt.show()
+    del table['Notes', 'Date_MJD', 'Pos_X', 'Pos_Y', 'Pos_Z', 'Performance_UsedVsRecov']
+    # A disgustingly hacky way to get a formatted table that I can write with reportlab, should revise to remove the need to generate a txt file.
+    ascii.write(table, 'table.tmp', format='fixed_width', overwrite=True)
+    with open('table.tmp', 'r') as f:
+        table_data = f.readlines()
+    generatePDF(output_name, start_time, stop_time, stat_code, tot_sess_str, tot_obs_str, wrms_str, perf_str, problems, table_data)
     return
     
 if __name__ == '__main__':
