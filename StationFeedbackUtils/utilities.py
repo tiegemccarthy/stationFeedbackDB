be2f298cf2ed1df7ac104e9b4e543ecf2c2f7288
Tiege McCarthy <tiegem@utas.edu.au>
2026-05-26 17:33:26 +1000

    renamed module directories

---
+++ StationFeedbackUtils/utilities.py
@@ -0,0 +1,41 @@
+import yaml             # pyyaml
+
+# debug
+# import json
+
+
+def stationParse(
+    stations_config,
+    reports=False
+):
+    """
+    Reads in configuration file for which stations to produce reports,
+    and returns lists of codes (short, two character station names)
+    and the longer station name codes.
+
+    Updated to take a .yaml file.
+
+    The 'reports' flag specifies whether to return the full list (when false)
+    (of stations to be processed into the database) or just the list of stations to create reports for,
+    as set by the existence of the 'report: true' line in the config (when true).
+
+    """
+
+    with open(stations_config) as file:
+        stations = yaml.safe_load(file)["stations"]
+
+    # initialise what we return
+    stationNames = []
+    stationNamesLong = []
+
+    # pull from the config
+    for code, info in stations.items():
+        if reports:
+            if ("report" not in info or not info["report"]):
+                # when generating reports, we ignore stations that do not have a report flag set to True (whether false or absent).
+                continue
+
+        stationNames.append(str(code))
+        stationNamesLong.append(str(info["name"]))
+
+    return stationNames, stationNamesLong
